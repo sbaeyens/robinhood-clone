76ce7d0cb316f8beca885206af2876fc34e76a29
Sean Baeyens <srbaeyens@gmail.com>
2023-03-23 17:38:49 -0700

    db fix for render

--- app/models/watchlist_stock.py
+++ app/models/watchlist_stock.py
@@ -2,6 +2,7 @@ from .db import db, SCHEMA, environment, add_prefix_for_prod
 
 watchlist_stocks = db.Table(
     'watchlist_stocks',
+    db.Model.metadata,
     db.Column('watchlist_id', db.Integer, db.ForeignKey(
         add_prefix_for_prod("watchlists.id")), primary_key=True),
     db.Column('ticker', db.String, db.ForeignKey(

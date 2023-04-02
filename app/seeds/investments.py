e54ae659c4904dd5406a46247938b109591f6900
Sean Baeyens <srbaeyens@gmail.com>
2023-04-02 15:16:12 -0700

    fixed seeders

--- app/seeds/investments.py
+++ app/seeds/investments.py
@@ -21,36 +21,32 @@ def seed_investments():
     )
 
     i4 = Investment(
-        portfolio_id= 1,
+        portfolio_id= 2,
         stock_id= "AMZN",
         quantity= 4,
     )
 
     i5 = Investment(
-        portfolio_id= 1,
+        portfolio_id= 2,
         stock_id= "AAPL",
         quantity= 3,
     )
 
     i6 = Investment(
-        portfolio_id= 1,
+        portfolio_id= 3,
         stock_id= "AMZN",
         quantity= 2,
     )
 
     i7 = Investment(
-        portfolio_id= 1,
+        portfolio_id= 3,
         stock_id= "GOOG",
         quantity= 5,
     )
 
-    i8 = Investment(
-        portfolio_id= 1,
-        stock_id= "GOOG",
-        quantity= 4,
-    )
 
-    all_investments = [i1, i2, i3, i4, i5, i6, i7, i8]
+
+    all_investments = [i1, i2, i3, i4, i5, i6, i7]
     add_investments = [db.session.add(investment) for investment in all_investments]
     db.session.commit()
 

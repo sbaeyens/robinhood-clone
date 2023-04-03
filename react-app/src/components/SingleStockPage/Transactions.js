67fe7bd23acc54ea38578e3c060c79773de7795c
Sean Baeyens <srbaeyens@gmail.com>
2023-04-02 20:51:56 -0700

    watchlist dropdown functionality added

--- react-app/src/components/SingleStockPage/Transactions.js
+++ react-app/src/components/SingleStockPage/Transactions.js
@@ -1,10 +1,11 @@
 import React from "react";
-
 import "./Transactions.css";
 // import { useParams, NavLink, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTransactionsByTicker } from "../../store/transactions";
+import { addCommas } from "../../Utils";
+
 
 
 
@@ -32,13 +33,18 @@ function Transactions({ ticker }) {
       {transactionsArray?.map((transaction) => (
         <div className="transaction-card" key={transaction.id}>
           <div>
-            <p>{transaction.transaction_type.toUpperCase() } - {transaction.stock_id}</p>
-            <p>{transaction.date}</p>
+            <p className="transaction-top-text">
+              {transaction.transaction_type} - {transaction.stock_id}
+            </p>
+            <p className="transaction-bottom-text">{transaction.date}</p>
           </div>
-          <div>
-            <p>{transaction.total_expense}</p>
-            <p>
-              {transaction.quantity} at {transaction.price_at_time}
+          <div className="transaction-summary">
+            <p className="transaction-top-text">
+              ${addCommas(Number(transaction.total_expense).toFixed(2))}
+            </p>
+            <p className="transaction-bottom-text">
+              {transaction.quantity} share(s) at $
+              {addCommas(Number(transaction.price_at_time).toFixed(2))}
             </p>
           </div>
         </div>
